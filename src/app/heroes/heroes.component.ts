96911ed793dffb2805fa0e1c201cecc52fb15d6f
Shayan Razavi <equinoxteam001@gmail.com>
2023-07-02 01:44:52 +1000

    pc transfer

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes : Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService : HeroService) { }
+
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
